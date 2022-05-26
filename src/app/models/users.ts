598add0cca77d3b45e1b1cdcdfb12ee4afa4b8e0
sheyenne de morais carvalho <sheyenne_22@hotmail.com>
2022-05-26 18:56:26 -0400

    refazendoUC10

--- src/app/models/users.ts
+++ src/app/models/users.ts
@@ -1,4 +1,7 @@
 export class Users {
+    subscribe(arg0: (response: any) => void): Users {
+      throw new Error('Method not implemented.');
+    }
     constructor(
             public email: string = "",
             public password?: string
